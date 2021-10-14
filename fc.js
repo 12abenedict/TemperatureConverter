21840fb0f056d9f8b1cf880dae809192179b2852
12abenedict <austin.stone2012@gmail.com>
2021-10-14 13:32:22 -0500

    v1 temperature converter. Need to remove NaN and work on design

---
+++ fc.js
@@ -0,0 +1,13 @@
+// When the input field receives input, convert the value from fahrenheit to celsius
+
+// Fahrenheit to Celsius
+function temperatureConverterFC(valNum) {
+    valNum = parseFloat(valNum);
+    document.getElementById("outputCelsius").innerHTML = (valNum-32) / 1.8;
+}
+
+// Celsius to Fahrenheit
+function temperatureConverterCF(valNum) {
+    valNum = parseFloat(valNum);
+    document.getElementById("outputFahrenheit").innerHTML = (valNum*1.8) + 32;
+} 
